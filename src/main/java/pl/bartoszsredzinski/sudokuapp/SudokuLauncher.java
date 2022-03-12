f653dd80c7696fdf413abb43e1301c9b5400c860
barti132 <barteksredzinski@gmail.com>
2022-03-12 16:09:35 +0100

    delete module

---
+++ src/main/java/pl/bartoszsredzinski/sudokuapp/SudokuLauncher.java
@@ -0,0 +1,13 @@
+package pl.bartoszsredzinski.sudokuapp;
+
+/**
+ * Class description
+ *
+ * @author Bartosz Średziński
+ * created on 12.03.2022
+ */
+public class SudokuLauncher{
+    public static void main(String[] args){
+        SudokuApp.main(args);
+    }
+}
